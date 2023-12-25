8c67c15686ee862c8d11ecd9c7b96564c1efc7e4
dadda  abdelghafour <dadda.abdelghafour2000@gmail.com>
2023-12-25 10:54:39 +0100

    Add Attribute `paginationTypes`

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@ import "./basic.css";
 const currentPage = ref(1);
 
 const clickCallback = (page: number) => {
-  // console.log(page);
   currentPage.value = page;
 };
 </script>
@@ -18,16 +17,13 @@ const clickCallback = (page: number) => {
     :boundaryCount="2"
     :siblingCount="2"
     :showFirstButton="true"
-    :showLastButton="true"
+    :showLastButton="false"
     :click-handler="clickCallback"
     type="link"
     linkUrl="{page}"
-    pagination-style="style1"
+    pagination-style="style3"
+    pagination-types="normal"
   >
-    <!-- pagination-types="of" -->
-    <!-- pagination-types="minimal" -->
-    <!-- pagination-types="standard" -->
-    <!-- pagination-types="normal" -->
     <template #firstBtn>
       <svg viewBox="0 0 24 24">
         <path d="M18.41 16.59 13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" />
@@ -55,17 +51,14 @@ const clickCallback = (page: number) => {
     disabled
     :boundaryCount="2"
     :siblingCount="2"
-    :showFirstButton="false"
+    :showFirstButton="true"
     :showLastButton="true"
     :click-handler="clickCallback"
     type="link"
     linkUrl="{page}"
-    pagination-style="style2"
-    pagination-types="normal"
+    pagination-style="style1"
+    pagination-types="of"
     >
-    <!-- pagination-types="minimal" -->
-    <!-- pagination-types="standard" -->
-    <!-- pagination-types="of" -->
     <template #firstBtn>
       <svg viewBox="0 0 24 24">
         <path d="M18.41 16.59 13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" />
@@ -99,11 +92,8 @@ const clickCallback = (page: number) => {
     type="link"
     linkUrl="{page}"
     pagination-style="style3"
+    pagination-types="standard"
   >
-    <!-- pagination-types="minimal" -->
-    <!-- pagination-types="of" -->
-    <!-- pagination-types="standard" -->
-    <!-- pagination-types="normal" -->ù
     <template #firstBtn>
       <svg viewBox="0 0 24 24">
         <path d="M18.41 16.59 13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" />
@@ -125,10 +115,41 @@ const clickCallback = (page: number) => {
       </svg>
     </template>
   </PaginateVuejs>
-
-  <h2>
-    <code>currentPage {{ currentPage }}</code>
-  </h2>
+  <br />
+  <PaginateVuejs
+    :total="200"
+    disabled
+    :boundaryCount="2"
+    :siblingCount="2"
+    :showFirstButton="false"
+    :showLastButton="true"
+    :click-handler="clickCallback"
+    type="link"
+    linkUrl="{page}"
+    pagination-style="style2"
+    pagination-types="minimal"
+    >
+    <template #firstBtn>
+      <svg viewBox="0 0 24 24">
+        <path d="M18.41 16.59 13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z" />
+      </svg>
+    </template>
+    <template #prevBtn>
+      <svg viewBox="0 0 24 24">
+        <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+      </svg>
+    </template>
+    <template #nextBtn>
+      <svg viewBox="0 0 24 24">
+        <path d="M10 6 8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+      </svg>
+    </template>
+    <template #lastBtn>
+      <svg viewBox="0 0 24 24">
+        <path d="M5.59 7.41 10.18 12l-4.59 4.59L7 18l6-6-6-6zM16 6h2v12h-2z" />
+      </svg>
+    </template>
+  </PaginateVuejs>
 </template>
 
 <style></style>
